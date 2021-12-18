85e4ba5e3322125ff95fda2d6a698501b77f400d
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2021-12-18 23:21:36 +0200

    navigationBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StartComponent } from './start/start.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StartComponent
+    StartComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
