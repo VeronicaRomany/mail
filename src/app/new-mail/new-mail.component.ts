3ef9291a6630efc11075e96bf8dd92aeec1b9ad0
MarkEhab33 <markoehab33@gmail.com>
2021-12-20 14:59:59 +0200

    class mail

--- src/app/new-mail/new-mail.component.ts
+++ src/app/new-mail/new-mail.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
-
+class NewMail {
+  sender: string;
+  subject:string;
+  text:string; 
+}
 @Component({
   selector: 'app-new-mail',
   templateUrl: './new-mail.component.html',
   styleUrls: ['./new-mail.component.css']
 })
+
 export class NewMailComponent implements OnInit {
 
   constructor() { }
