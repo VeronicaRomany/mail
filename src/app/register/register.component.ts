536d3cc0a6cdc1a5a1ac5ab9dabaea98679e327c
AntoineTalaat <es-antoinetalaat2024@alexu.edu.eg>
2021-12-26 19:09:10 +0200

    post request

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -29,12 +29,13 @@ gotToLogin(){
 
 check(){
   const body = ((document.getElementById("newMember") as HTMLInputElement).value);
-  this.http.post<boolean>('http://localhost:8080/server/user/register', body).subscribe(data =>{
-      var success=data
-      if(data){
-        this.http.post<string>('http://localhost:8080/server/user/login', body).subscribe(data =>{
-          var id = data
-          console.log(id)
+  this.http.post<boolean>('http://localhost:8080/server/user/register', body).subscribe(next =>{
+    console.log(next)
+    if(next){
+         this.http.post('http://localhost:8080/server/user/login',body,{responseType:'text'}).subscribe(response =>{
+          //var id= JSON.stringify(response)
+
+          console.log(response)
       });
      
   }
