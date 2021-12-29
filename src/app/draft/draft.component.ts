15101c7d0314602ffc1feab2bfb09673cf1d7492
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2021-12-29 15:19:59 +0200

    draft

--- src/app/draft/draft.component.ts
+++ src/app/draft/draft.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,  NavigationExtras, Router } from '@angular/router';
 import { NewMail } from '../table/table.component';
 import { Globals } from 'src/globals';
+import { filter } from '../table/table.component';
 @Component({
   selector: 'app-draft',
   templateUrl: './draft.component.html',
@@ -20,9 +21,172 @@ export class DraftComponent implements OnInit {
 
  
  
- isSomethingSelected:boolean=false;
+ select :boolean=false;
  selected:any=[]
+ senderFlag:boolean=false;
+ recieverFlag:boolean=false;
+ subjectFlag:boolean=false;
+ priorityFlag:boolean=false;
+ confirmsenderFlag:boolean=false;
+ confirmrecieverFlag:boolean=false;
+ confirmsubjectFlag:boolean=false;
+ confirmpriorityFlag:boolean=false;
+ filter:boolean=false;
+ sortSelector:string="";
+ isSomethingSelected:boolean=false;
 
+ 
+ SortBy(){
+  this.sortSelector= ((document.getElementById("sort") as HTMLInputElement).value);
+  console.log(this.sortSelector)
+  this.http.get("http://localhost:8080/server/mail/sort",{responseType:'text',
+  params:{
+    userID:this.globals.userID,
+    folder:"draft",
+    criteria: this.sortSelector
+  },observe:'response'
+
+  }).subscribe((data:any) =>{
+    console.log(data.body)
+    this.drafts=[]
+   var jsonstr:string=data.body;
+   let jsonArr=JSON.parse(jsonstr)
+   console.log(jsonArr)
+   for(var i in jsonArr){
+     this.drafts.push(jsonArr[i])
+    console.log(this.drafts)
+   }
+  })
+}
+
+filterBy(){
+  this.filter=!this.filter;
+  if(this.filter){
+  this.senderFlag=true;
+  this.subjectFlag=true;
+  this.priorityFlag=true;
+  this.recieverFlag=true;
+  console.log("on")
+  }else{
+    this.stopFilter()
+    console.log("off")
+  }
+  
+}
+filterConfirmation(){
+  var sender;
+  var subject;
+  var pr;
+  var priority;
+  var reciever;
+ 
+
+  if(this.confirmrecieverFlag){
+    reciever= ((document.getElementById("recieverFilter") as HTMLInputElement).value);
+   
+  }else{
+    reciever=""
+  }
+  
+  if(this.confirmsubjectFlag){
+    subject= ((document.getElementById("subjectFilter") as HTMLInputElement).value);
+   
+  }else{
+    subject=""
+  }
+  
+  if(this.confirmpriorityFlag){
+      priority= ((document.getElementById("priority") as HTMLInputElement).value);
+    
+    if(priority=="Urgent"){
+      pr=4
+    }else if(priority=="High"){
+      pr=3
+    }else if(priority=="Medieum"){
+      pr=2
+    }else{
+      pr=1
+    }
+   
+   
+  }else{
+    pr=0
+  }
+ 
+  let f = new filter()
+  f.subject=subject
+ 
+  f.priority=pr
+  f.receiver=reciever
+  console.log(f)
+
+  var x= JSON.stringify(f)
+  console.log(x)
+  this.http.get("http://localhost:8080/server/mail/filter",{responseType:'text',
+  params:{
+    userID:this.globals.userID,
+    folder:"draft",
+    filtersJSON: x
+  },observe:'response'
+
+  }).subscribe((data:any) =>{
+    console.log(data.body)
+  
+   var jsonstr:string=data.body;
+   let jsonArr=JSON.parse(jsonstr)
+   console.log(jsonArr)
+   this.drafts=[]
+   for(var i in jsonArr){
+     this.drafts.push(jsonArr[i])
+    console.log(this.drafts)
+   }
+  })
+}
+
+stopFilter(){
+  this.confirmpriorityFlag=false;
+  this.confirmsenderFlag=false;
+  this.confirmsubjectFlag=false;
+  this.confirmrecieverFlag=false;
+  this.senderFlag=false;
+  this.subjectFlag=false;
+  this.priorityFlag=false;
+  this.recieverFlag=false;
+  this.filter=false;
+  this.drafts=[]
+  this.getdraft()
+}
+joinFilterBySender(event: any){
+  if (event.target.checked) {
+    this.confirmsenderFlag=true;
+}else{
+  this.confirmsenderFlag=false;
+}
+}
+
+joinFilterByReciever(event: any){
+if (event.target.checked) {
+  this.confirmrecieverFlag=true;
+}else{
+this.confirmrecieverFlag=false;
+}
+}
+joinFilterBySubject(event: any){
+  if (event.target.checked) {
+    this.confirmsubjectFlag=true
+  }else{
+    this.confirmsubjectFlag=false;
+  }
+}
+
+joinFilterByPriority(event: any){
+  if (event.target.checked) {
+
+    this.confirmpriorityFlag=true
+ }else{
+  this.confirmpriorityFlag=false;
+ }
+}
 
  deleteSelected(){
   // send array selected f request
@@ -79,6 +243,7 @@ export class DraftComponent implements OnInit {
   ngOnInit(): void {
   }
   getdraft(){
+    this.drafts=[]
     console.log("sasasas")
     this.http.get("http://localhost:8080/server/user/getMailFolder",{responseType:'text',
     params:{
@@ -200,7 +365,8 @@ export class DraftComponent implements OnInit {
        });
    }
   
-  goto(pr:string,to:string , subject:string ,mail:string){
+  goto(pr:string,to:string , subject:string ,mail:string , id:any){
+    this.delete(id)
    // this.router.navigate(['/NewMail'], { fragment:mail });
    this.router.navigate(['/NewMail'], { state: {importance:pr,reciever:to,head:subject, Mail: mail }})
   }
